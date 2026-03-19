37a3ea63de2bac48c35ee05c52d6972b192cb028
박경일 <one040422@gmail.com>
2026-03-19 17:45:10 +0900

    과제1 코딩과 테스트 완료 listbox와 textbox를 배치함 textbox에 내용 입력후 전송버튼 클릭이나 enter키를 누르면 listbox에 이동후 textbox 내용은 지워

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,13 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private Button button1;
+        private Label label1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +18,65 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(623, 357);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 42);
+            button1.TabIndex = 0;
+            button1.Text = "전송";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonFace;
+            label1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(88, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(177, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Echo Messenger";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(88, 59);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(622, 274);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 368);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(502, 23);
+            textBox1.TabIndex = 3;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
-
-        #endregion
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -5,6 +8,35 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // 버튼 클릭 이벤트
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        // Enter 키 입력 이벤트
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendMessage();
+                e.SuppressKeyPress = true; // 삑 소리 방지
+            }
+        }
+
+        // 메시지 전송 공통 로직
+        private void SendMessage()
+        {
+            string message = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(message))
+            {
+                listBox1.Items.Add(message); // ListBox에 추가
+                textBox1.Clear();            // TextBox 비우기
+                textBox1.Focus();            // 커서 다시 TextBox로
+                listBox1.TopIndex = listBox1.Items.Count - 1; // 자동 스크롤
+            }
         }
     }
 }
